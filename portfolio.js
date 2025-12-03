7ed60c95966a7c2f86c7392000c839cc65ebc449
Mhangwani Ndima Karabo <203499810+Ndima-karabo17@users.noreply.github.com>
2025-12-03 07:41:03 +0200

    Update portfolio.js
    I change cv from writing my name because it wasn't getting it

--- portfolio.js
+++ portfolio.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-     const hireButton = document.getElementById('hire');
+   
+    const hireButton = document.getElementById('hire');
     
     const handleDownloadCV = () => {
         const downloadLink = document.createElement('a');
         downloadLink.href = 'cv.pdf'; 
-        downloadLink.download = 'Ndima_Mhangwani.pdf'; 
+        downloadLink.download = 'cv.pdf'; 
         
         document.body.appendChild(downloadLink);
         downloadLink.click();
@@ -18,47 +19,10 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
         console.warn("Element with ID 'hire' not found. Download button functionality disabled.");
     } 
-  
-    const themeToggle = document.getElementById('theme-toggle');
-    const themeIcon = document.getElementById('theme-icon');
-    const htmlElement = document.documentElement;
-
-  
-    const storedTheme = localStorage.getItem('theme');
-    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    
-    let initialDarkMode = storedTheme === 'dark' || (!storedTheme && prefersDark);
-
-    if (initialDarkMode) {
-        htmlElement.classList.add('dark');
-        themeIcon.classList.remove('fa-moon');
-        themeIcon.classList.add('fa-sun');
-    } else {
-        htmlElement.classList.remove('dark');
-        themeIcon.classList.remove('fa-sun');
-        themeIcon.classList.add('fa-moon');
-    }
-
-    if (themeToggle) {
-        themeToggle.addEventListener('click', () => {
-            htmlElement.classList.toggle('dark');
-
-          
-            if (htmlElement.classList.contains('dark')) {
-                themeIcon.classList.remove('fa-moon');
-                themeIcon.classList.add('fa-sun');
-                localStorage.setItem('theme', 'dark');
-            } else {
-                themeIcon.classList.remove('fa-sun');
-                themeIcon.classList.add('fa-moon');
-                localStorage.setItem('theme', 'light');
-            }
-        });
-    }
-
 
     const currentYearElement = document.getElementById('currentYear');
     if (currentYearElement) {
         currentYearElement.textContent = new Date().getFullYear();
     }
+    
 });
