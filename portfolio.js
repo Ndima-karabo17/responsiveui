9799a057f174ef1c6010b9adbb6a9a560fcf5482
Mhangwani Ndima Karabo <203499810+Ndima-karabo17@users.noreply.github.com>
2026-01-19 06:26:54 +0200

    Update portfolio.js
    I added functionality for dark and light mode

--- portfolio.js
+++ portfolio.js
@@ -26,3 +26,80 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("portfolio-form");
+    const status = document.getElementById("form-status");
+    const button = document.getElementById("submit-button");
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        
+        const data = new FormData(event.target);
+        
+    
+        button.disabled = true;
+        button.innerHTML = '<i class="fas fa-circle-notch animate-spin"></i> Sending...';
+
+        fetch(event.target.action, {
+            method: form.method,
+            body: data,
+            headers: {
+                'Accept': 'application/json'
+            }
+        }).then(response => {
+            if (response.ok) {
+                
+                status.style.display = "flex";
+                form.reset();
+            } else {
+              
+                response.json().then(data => {
+                    if (Object.hasOwn(data, 'errors')) {
+                        alert(data["errors"].map(error => error["message"]).join(", "));
+                    } else {
+                        alert("Oops! There was a problem submitting your form");
+                    }
+                });
+            }
+        }).catch(error => {
+            
+            alert("Oops! There was a problem connecting to the server.");
+        }).finally(() => {
+            
+            button.disabled = false;
+            button.innerHTML = "Send Message";
+        });
+    }
+
+    form.addEventListener("submit", handleSubmit);
+});
+
+//dark and light mode
+
+
+  const themeToggleBtn = document.getElementById('theme-toggle');
+        const darkIcon = document.getElementById('theme-toggle-dark-icon');
+        const lightIcon = document.getElementById('theme-toggle-light-icon');
+
+  
+        if (localStorage.getItem('color-theme') === 'light' || (!('color-theme' in localStorage) && !window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.remove('dark');
+            darkIcon.classList.remove('hidden');
+        } else {
+            document.documentElement.classList.add('dark');
+            lightIcon.classList.remove('hidden');
+        }
+
+        themeToggleBtn.addEventListener('click', function() {
+        
+            document.documentElement.classList.toggle('dark');
+            darkIcon.classList.toggle('hidden');
+            lightIcon.classList.toggle('hidden');
+
+        
+            if (document.documentElement.classList.contains('dark')) {
+                localStorage.setItem('color-theme', 'dark');
+            } else {
+                localStorage.setItem('color-theme', 'light');
+            }
+        });
